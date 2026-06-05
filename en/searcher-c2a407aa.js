444a1d13a6479fdd94db577e8ddc639304ce6f0e
robguthrie <robguthrie@users.noreply.github.com>
2026-06-05 03:25:01 +0000

    deploy: 701ff6afdf8454a01bfb961a0711f06c56b56492

--- en/searcher-c2a407aa.js
+++ en/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ff77582b.js',
+                path_to_root + 'searchindex-4d9dcb56.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
